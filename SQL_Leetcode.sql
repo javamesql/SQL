0827582c012f1a654fc79433d19cbe8d93050db3
James D <141413672+javamesql@users.noreply.github.com>
2023-11-17 03:07:24 -0800

    Update SQL_Leetcode.sql

--- SQL_Leetcode.sql
+++ SQL_Leetcode.sql
@@ -28,3 +28,28 @@ WHERE
     3 > (SELECT COUNT(DISTINCT E2.salary)
         FROM Employee E2
         WHERE E2.salary > E.salary AND E.departmentId = E2.departmentId);
+
+--Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who: have the same tiv_2015 value as one or more other policyholders, 
+--and are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique). Round tiv_2016 to two decimal places.
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN
+    (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*) > 1)
+    AND (lat,lon) IN (SELECT lat,lon FROM Insurance GROUP BY lat,lon HAVING COUNT(*) = 1);
+
+--Write a solution to calculate the number of bank accounts for each salary category. The salary categories are: 
+--"Low Salary": All the salaries strictly less than $20000.
+--"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+--"High Salary": All the salaries strictly greater than $50000.
+(SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count 
+FROM accounts 
+WHERE income < 20000)
+UNION
+(SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count 
+FROM accounts 
+WHERE income BETWEEN 20000 AND 50000)
+UNION
+(SELECT 'High Salary' AS category, COUNT(*) AS accounts_count 
+FROM accounts 
+WHERE income > 50000);
+
