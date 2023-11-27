7532714dfa2e8a444c5a0c3d01ec8edfe9e23e3f
James D <141413672+javamesql@users.noreply.github.com>
2023-11-26 20:26:46 -0800

    Update SQL_Leetcode.sql

--- SQL_Leetcode.sql
+++ SQL_Leetcode.sql
@@ -68,3 +68,17 @@ LEFT JOIN Weather B
     ON DATE(A.recordDate) = DATE(B.recordDate + INTERVAL 1 DAY)
 WHERE A.temperature > B.temperature;
 
+--There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+--The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+--The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM Activity a1
+JOIN Activity a2
+ON a1.machine_id = a2.machine_id
+    AND a1.process_id = a2.process_id
+    AND a1.activity_type = 'start'
+    AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
+
+
+
